f3f8fd72cf348bb60a1d0cadf91e35f1819a1607
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2024-12-24 11:53:21 -0300

    feat: alterado nome da challenge 03-02 para 03-12 e adicionado 11-12

--- coddy_03-12-2024/ForestHikePathTracker.java
+++ coddy_03-12-2024/ForestHikePathTracker.java
@@ -11,7 +11,7 @@ class ForestHikePathTracker {
   }
   public static void main(String[] args) {
     int[] elevations = {1,2,3,4,5};
-    //System.out.println(ForestHikePathTracker.forestHikePathTracker(elevations)); só assim mostrará um hashcode
-    System.out.println(Arrays.toString(ForestHikePathTracker.forestHikePathTracker(elevations))); // assim converte a informação do array para string.
+    //System.out.println(ForestHikePathTracker.forestHikePathTracker(elevations)); Se colocar assim, mostrará um hashcode.
+    System.out.println(Arrays.toString(ForestHikePathTracker.forestHikePathTracker(elevations))); // Desse jeito, converte a informação do array para string.
   }
 }
